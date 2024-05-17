200365a9fd81fe021a28f234e8723599da41a1c7
Hieu <vuminhhieu05122004@gmail.com>
2024-05-17 22:01:39 +0700

    add media class, create disc class

--- Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/disc/DigitalVideoDisc.java
@@ -1,76 +1,27 @@
 package hust.soict.dsai.aims.disc;
 
-public class DigitalVideoDisc {
-	private String title;
-	private String category;
-	private String director;
-	private int length;
-	private float cost;
-	private static int nbDigitalVideoDiscs = 0;
-	private int id;
-	
-	public String getTitle() {
-		return title;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public String getDirector() {
-		return director;
-	}
-	public int getLength() {
-		return length;
-	}
-	public float getCost() {
-		return cost;
-	}
+import hust.soict.dsai.aims.disc.Disc;
+
+public class DigitalVideoDisc extends Disc{
 	public DigitalVideoDisc(String title) {
-		super();
-		this.title = title;
-		nbDigitalVideoDiscs = nbDigitalVideoDiscs + 1;
-		this.setId(nbDigitalVideoDiscs);
+		super(title);
 	}
 	public DigitalVideoDisc(String title, String category, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.cost = cost;
-		nbDigitalVideoDiscs = nbDigitalVideoDiscs + 1;
-		this.setId(nbDigitalVideoDiscs);
+		super(title, category, cost);
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.cost = cost;
-		nbDigitalVideoDiscs = nbDigitalVideoDiscs + 1;
-		this.setId(nbDigitalVideoDiscs);
+		super(title, category,director, cost);
 	}
+	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-		super();
-		this.title = title;
-		this.category = category;
-		this.director = director;
-		this.length = length;
-		this.cost = cost;
-		nbDigitalVideoDiscs = nbDigitalVideoDiscs + 1;
-		this.setId(nbDigitalVideoDiscs);
-	}
-	public void setTitle(String title) {
-		this.title = title;
+		super(title, category, director, length, cost);
 	}
 	public String toString() {
-		String lmao = "DVD - " + this.title + " - " + this.category
-				+ " - " + this.director + " - " + this.length 
-				+ ": " + this.cost;
+		String lmao = "DVD - " + this.getTitle() + " - " + this.getCategory()
+				+ " - " + this.getDirector() + " - " + this.getLength() 
+				+ ": " + this.getCost();
 		return lmao;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	
 }
---
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/disc/Track.java
@@ -0,0 +1,24 @@
+package hust.soict.dsai.aims.disc;
+
+public class Track {
+	private String title;
+	private int length;
+	
+	public Track() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+}
